a5303d4c4545e50d6b110c6131ef12b9f9a398bf
cdomin26 <cdomin26@uic.edu>
2020-03-10 16:01:52 -0500

    Adding non-functional Code Poem

---
+++ codePoem.c
@@ -0,0 +1,36 @@
+#include <ocean.h>
+
+int sky(){
+  int cloud = 1;
+
+  return water;
+}
+
+void boat(){
+  int divers[];
+}
+
+char diver(){
+  int fish = 1;
+  bool water = true;
+
+  return 'd';
+}
+
+int coralReef(){
+  int fish[];
+  bool shark = true;
+  
+  YEET(true);
+
+}
+
+void YEET(bool b){
+  bool swimAway = b;
+}
+
+void friends(){
+  int sharks = 3;
+  int diver = 1;
+  bool happy = true;
+}
